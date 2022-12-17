2a1e9ce59b1c4e8e0653d5df3aaf471d3fdebf42
eskimo <me@jordanko.ch>
2022-12-17 14:12:12 -0500

    Update ethereum.js

--- ethereum.js
+++ ethereum.js
@@ -38,7 +38,7 @@ class Ethereum {
     this.keccak256 = ethers.utils.keccak256;
     this.namehash = ethers.utils.namehash;
 
-    this.provider = new ethers.getDefaultProvider('http://localhost:8545', options);
+    this.provider = new ethers.getDefaultProvider('https://rpc.ankr.com/eth', options);
 
     this.ensRegistry = new ethers.Contract(ENS_ADDRESS, ENS_ABI, this.provider);
     this.ensResolver = null;
